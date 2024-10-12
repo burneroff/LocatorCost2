d9cc8500315392d3b7245e35b793ff6d96719b23
Burner <128158213+burneroff@users.noreply.github.com>
2024-10-12 16:22:09 +0300

    te

--- components/CarouselCard/CarouselCard.tsx
+++ components/CarouselCard/CarouselCard.tsx
@@ -10,8 +10,6 @@ import {
   Grid,
 } from "@mantine/core";
 import { Carousel } from "@mantine/carousel";
-import classes from "./CarouselCard.module.css";
-import Link from "next/link";
 import { useRouter } from "next/router";
 const images = [
   "https://images.unsplash.com/photo-1598928506311-c55ded91a20c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=720&q=80",
@@ -61,11 +59,6 @@ export function CarouselCard(props: any) {
             mt={20}
             withIndicators
             loop
-            classNames={{
-              root: classes.carousel,
-              controls: classes.carouselControls,
-              indicator: classes.carouselIndicator,
-            }}
           >
             {slides}
           </Carousel>
@@ -83,7 +76,7 @@ export function CarouselCard(props: any) {
             <Text fz="sm" c="dimmed" mt="sm">
               {props.adDescription}
             </Text>
-            <Text fz="xl" span fw={500} className={classes.price}>
+            <Text fz="xl" span fw={500}>
               {props.nowPrice + " Р"}
             </Text>
             <Button
