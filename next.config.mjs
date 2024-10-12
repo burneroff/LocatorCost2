3f784ca76d83902c98338b409435703eea5b81f8
Burner <128158213+burneroff@users.noreply.github.com>
2024-10-12 18:54:19 +0300

    test

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://185.251.91.245:5000/api/:path*', // Прокси на ваш сервер
+      },
+    ];
+  },
 };
 
 export default nextConfig;
